36649c40d9ed1d12789293b8bff6c20eba93a561
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-02-17 16:50:56 +0100

    movie class added

---
+++ src/main/java/movie/Movie.java
@@ -0,0 +1,51 @@
+package movie;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Movie
+{
+    private String title;
+
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private Genre genre;
+
+    private BigDecimal price;
+
+    public Movie(String title, String description, LocalDate releaseDate, Genre genre, BigDecimal price)
+    {
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.genre = genre;
+        this.price = price;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public LocalDate getReleaseDate()
+    {
+        return releaseDate;
+    }
+
+    public Genre getGenre()
+    {
+        return genre;
+    }
+
+    public BigDecimal getPrice()
+    {
+        return price;
+    }
+}
