773ff826351f1458177dbb4644868c1b98626cb0
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-02-17 16:50:38 +0100

    enum with movie genre

---
+++ src/main/java/movie/Genre.java
@@ -0,0 +1,23 @@
+package movie;
+
+public enum Genre
+{
+    ACTION,
+    ADVENTURE,
+    ANIMATION,
+    COMEDY,
+    CRIME,
+    DOCUMENTARY,
+    DRAMA,
+    FAMILY,
+    FANTASY,
+    HISTORY,
+    HORROR,
+    MUSIC,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER,
+    WAR,
+    WESTERN
+}
