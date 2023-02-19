4e29c4a53e19a5d5164204b6ded2cf0fdaa58a3e
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-02-19 23:25:12 +0100

    all method done and tested

---
+++ src/main/java/movie/collection/MovieCollection.java
@@ -0,0 +1,132 @@
+package movie.collection;
+
+import movie.Movie;
+import movie.genre.Genre;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class MovieCollection
+{
+    private static Long key = 0L;
+
+    private static Map<Long, Movie> idToMovie = new HashMap<>();
+
+    private static Path moviesFilePath = Path.of("movies.txt");
+
+    public static void load()
+    {
+        idToMovie.clear();
+        key = 0L;
+        Stream<String> lines = getStreamOfLines(moviesFilePath);
+        lines.forEach(line ->
+        {
+            String[] parts = line.split("\\|");
+            Movie movie = new Movie(parts[0], parts[1], LocalDate.parse(parts[2], DateTimeFormatter.ofPattern("yyyy-MM-dd")), Genre.valueOf(parts[3].toUpperCase()), new BigDecimal(parts[4]));
+            add(movie);
+        });
+        System.out.println("Movies has been loaded");
+    }
+
+    private static Stream<String> getStreamOfLines(Path path)
+    {
+        Stream<String> lines = null;
+        try
+        {
+            lines = Files.lines(path);
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+        return lines;
+    }
+
+    public static void save()
+    {
+        try (PrintWriter moviesFileWriter = new PrintWriter(Files.newBufferedWriter(moviesFilePath)))
+        {
+            idToMovie
+                    .values()
+                    .stream()
+                    .map(Movie::toString)
+                    .forEach(moviesFileWriter::println);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("Movies has been saved");
+    }
+
+    public static Optional<Movie> get(Long id)
+    {
+        return Optional.ofNullable(idToMovie.get(id));
+    }
+
+    public static Optional<Movie> get(String title)
+    {
+        return findByTitle(title);
+    }
+
+    public static void add(Movie movie)
+    {
+        idToMovie.put(key, movie);
+        key++;
+        System.out.println("Added new movie \"" + movie.getTitle() + "\".");
+    }
+
+    public static int getNumberOfMovies()
+    {
+        return idToMovie.size();
+    }
+
+    public static boolean remove(Long id)
+    {
+        Optional<Movie> removedMovie = Optional.ofNullable(idToMovie.remove(id));
+        if (removedMovie.isPresent())
+        {
+            System.out.println("Removed movie \"" + removedMovie.get().getTitle() + "\".");
+            return true;
+        }
+        System.out.println("There is no movie with this id.");
+        return false;
+    }
+
+    public static boolean remove(String title)
+    {
+        Optional<Movie> removedMovie = findByTitle(title);
+        if (removedMovie.isPresent())
+        {
+            System.out.println("Removed movie \"" + removedMovie.get().getTitle() + "\".");
+            return true;
+        }
+        System.out.println("There is no movie with that title.");
+        return false;
+    }
+
+    private static Optional<Movie> findByTitle(String title)
+    {
+        return idToMovie
+                .values()
+                .stream()
+                .filter(movie -> titlesAreTheSame(movie, title))
+                .findFirst();
+    }
+
+    private static boolean titlesAreTheSame(Movie movie, String title)
+    {
+        String lowercaseTitle = title.toLowerCase();
+        return movie.getTitle().toLowerCase().equals(lowercaseTitle);
+    }
+}
---
+++ src/test/java/movie/collection/MovieCollectionTest.java
@@ -0,0 +1,111 @@
+package movie.collection;
+
+import movie.Movie;
+import movie.genre.Genre;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieCollectionTest
+{
+    @BeforeAll
+    public static void initialize()
+    {
+        MovieCollection.load();
+    }
+
+    @Test
+    public void newMovieShouldBeAdded()
+    {
+        MovieCollection.add(new Movie("Test", "test", LocalDate.now(), Genre.ACTION, BigDecimal.ZERO));
+    }
+
+    @Test
+    @DisplayName("After adding movie their number should increment")
+    public void afterAddingMovieTheirNumberShouldIncrement()
+    {
+        int numberOfMovieBeforeAddingNew = MovieCollection.getNumberOfMovies();
+        MovieCollection.add(new Movie("Test", "test", LocalDate.now(), Genre.ACTION, BigDecimal.ZERO));
+        int numberOfMovieAfterAddingNew = MovieCollection.getNumberOfMovies();
+        assertEquals(numberOfMovieAfterAddingNew, numberOfMovieBeforeAddingNew + 1);
+    }
+
+    @Test
+    @DisplayName("Attempting to delete a movie by title that doesn't exist should return false")
+    public void attemptingToDeleteMovieByTitleThatDoesNotExistShouldReturnFalse()
+    {
+        boolean movieHasBeenDeleted = MovieCollection.remove("Non-existent movie title");
+        assertFalse(movieHasBeenDeleted);
+    }
+
+    @Test
+    @DisplayName("Attempting to delete a movie by title that exist should return true")
+    public void attemptingToDeleteMovieByTitleThatExistShouldReturnTrue()
+    {
+        Movie movie = MovieCollection.get(0L).get();
+        boolean movieHasBeenDeleted = MovieCollection.remove(movie.getTitle());
+        assertTrue(movieHasBeenDeleted);
+    }
+
+    @Test
+    @DisplayName("Attempting to delete a movie by id that doesn't exist should return false")
+    public void attemptingToDeleteMovieByIdThatDoesNotExistShouldReturnFalse()
+    {
+        boolean movieHasBeenDeleted = MovieCollection.remove(Long.MAX_VALUE);
+        assertFalse(movieHasBeenDeleted);
+    }
+
+    @Test
+    @DisplayName("Attempting to delete a movie by id that exist should return true")
+    public void attemptingToDeleteMovieByIdThatExistShouldReturnTrue()
+    {
+        boolean movieHasBeenDeleted = MovieCollection.remove(1L);
+        assertTrue(movieHasBeenDeleted);
+    }
+
+    @Test
+    @DisplayName("Attempting to get movie by id that doesn't exists should return empty optional object")
+    public void attemptingToGetMovieByIdThatDoesNotExistShouldReturnEmptyOptionalObject()
+    {
+        Optional<Movie> movie = MovieCollection.get(Long.MAX_VALUE);
+        assertTrue(movie.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Attempting to get movie by id that  exists should return movie optional object")
+    public void attemptingToGetMovieByIdThatExistShouldReturnMovieOptionalObject()
+    {
+        Optional<Movie> movie = MovieCollection.get(5L);
+        assertTrue(movie.isPresent());
+    }
+
+    @Test
+    @DisplayName("Attempting to get movie by title that doesn't exists should return empty optional object")
+    public void attemptingToGetMovieByTitleThatDoesNotExistShouldReturnEmptyOptionalObject()
+    {
+        Optional<Movie> movie = MovieCollection.get("Non-existent movie title");
+        assertTrue(movie.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Attempting to get movie by title that  exists should return movie optional object")
+    public void attemptingToGetMovieByTitleThatExistShouldReturnMovieOptionalObject()
+    {
+        Optional<Movie> movie = MovieCollection.get("The Godfather");
+        assertTrue(movie.isPresent());
+    }
+
+    @Test
+    @DisplayName("Save method should work without throwing errors")
+    public void saveMethodShouldWorkWithoutThrowingErrors()
+    {
+        MovieCollection.load();//We need to load movies from file because we don't want save any changes in file done in previous tests
+        MovieCollection.save();
+    }
+}
