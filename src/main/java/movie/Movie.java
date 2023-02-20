a5ae4bfc7453acec0785c44dd836db9c62b3dab0
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2023-02-20 02:30:25 +0100

    variable duration added getter for duration added

--- src/main/java/movie/Movie.java
+++ src/main/java/movie/Movie.java
@@ -3,6 +3,7 @@ package movie;
 import movie.genre.Genre;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
 
 public class Movie
@@ -17,13 +18,16 @@ public class Movie
 
     private BigDecimal price;
 
-    public Movie(String title, String description, LocalDate releaseDate, Genre genre, BigDecimal price)
+    private Integer duration;
+
+    public Movie(String title, String description, LocalDate releaseDate, Genre genre, BigDecimal price, Integer duration)
     {
         this.title = title;
         this.description = description;
         this.releaseDate = releaseDate;
         this.genre = genre;
         this.price = price;
+        this.duration = duration;
     }
 
     public String getTitle()
@@ -51,9 +55,14 @@ public class Movie
         return price;
     }
 
+    public Integer getDuration()
+    {
+        return duration;
+    }
+
     @Override
     public String toString()
     {
-        return title + '|' + description + '|' + releaseDate.toString() + '|' + genre.toString() + '|' + price.toString();
+        return title + '|' + description + '|' + releaseDate.toString() + '|' + genre.toString() + '|' + price.toString() + '|' + duration;
     }
 }
