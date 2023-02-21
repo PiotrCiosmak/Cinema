cd706a3b937a091d90dbfee46f6db72bce1af906
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-21 19:04:17 +0100

    tests repaired, now tests delete files after run

--- src/test/java/repertoire/collection/RepertoireCollectionTest.java
+++ src/test/java/repertoire/collection/RepertoireCollectionTest.java
@@ -2,14 +2,10 @@ package repertoire.collection;
 
 import movie.Movie;
 import movie.collection.MovieCollection;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.*;
 
-import javax.swing.plaf.PanelUI;
+import java.io.File;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -23,10 +19,26 @@ class RepertoireCollectionTest
 {
     private RepertoireCollection repertoireCollection;
 
+    private static List<String> repertoireFilePaths;
+
+    private final static LocalDate date = LocalDate.of(2000, 01, 01);
+
     @BeforeAll
     public static void initialize()
     {
+        loadRepertoireFilePaths();
         MovieCollection.load();
+    }
+
+    private static void loadRepertoireFilePaths()
+    {
+        LocalDate currentDate = date;
+        repertoireFilePaths = new ArrayList<>();
+        for (int i = 0; i < 7; ++i)
+        {
+            repertoireFilePaths.add("files/repertoire/" + currentDate + ".txt");
+            currentDate = currentDate.plusDays(1);
+        }
     }
 
     @RepeatedTest(10)
@@ -34,12 +46,12 @@ class RepertoireCollectionTest
     public void shouldGenerateRepertoireForWholeWeek()
     {
         List<LocalTime> workingHours = generateWorkingHours();
-        repertoireCollection = new RepertoireCollection(workingHours.get(0), workingHours.get(1));
+        repertoireCollection = new RepertoireCollection(workingHours.get(0), workingHours.get(1), date);
         List<Map<LocalDateTime, Movie>> repertoires = repertoireCollection.generate();
         assertFalse(repertoires.isEmpty());
     }
 
-    static private List<LocalTime> generateWorkingHours()
+    private static List<LocalTime> generateWorkingHours()
     {
         List<LocalTime> workingHours = new ArrayList<>(2);
         workingHours.add(generateOpeningOrClosingTime(6));
@@ -52,5 +64,18 @@ class RepertoireCollectionTest
         int openingHour = ThreadLocalRandom.current().nextInt(6) + earliestHour;
         int openingMinute = ThreadLocalRandom.current().nextInt(60);
         return LocalTime.of(openingHour, openingMinute);
+    }
+
+    @AfterEach
+    void cleanUp()
+    {
+        for (String repertoireFilePath : repertoireFilePaths)
+        {
+            File repertoireFile = new File(repertoireFilePath);
+            if (repertoireFile.exists())
+            {
+                repertoireFile.delete();
+            }
+        }
     }
 }
