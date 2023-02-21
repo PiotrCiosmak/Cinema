b5685a2b0bec2fe03c4eae9c930cce1485066e73
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-21 17:49:33 +0100

    Repertoire class done and tested

--- src/main/java/repertoire/Repertoire.java
+++ src/main/java/repertoire/Repertoire.java
@@ -11,49 +11,81 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 public class Repertoire
 {
-
-    private Long numberOfAvailableMinutes;
+    private Long numberOfMinutesAvailable;
 
     private LocalDateTime startTimeOfTheNextMovie;
 
-    private LocalDate repertoireValidityDate;
-
-    private Path repertoireDayPath;
+    private final Path repertoireFilePath;
 
     private Map<LocalDateTime, Movie> dateAndTimeToMovie;
 
     public Repertoire(LocalDate date, LocalTime openingHours, LocalTime closingHours)
     {
-        numberOfAvailableMinutes = Duration.between(openingHours, closingHours).toMinutes();
-        startTimeOfTheNextMovie = date.atTime(openingHours);
-        repertoireValidityDate = date;
-        repertoireDayPath = Path.of("files/repertoire/" + date + ".txt");
+        this.numberOfMinutesAvailable = Duration.between(openingHours, closingHours).toMinutes();
+        this.startTimeOfTheNextMovie = date.atTime(openingHours);
+        this.repertoireFilePath = Path.of("files/repertoire/" + date + ".txt");
         this.dateAndTimeToMovie = new HashMap<>();
     }
 
-    public void generate()
+    public Map<LocalDateTime, Movie> generate()
     {
         if (repertoireAlreadyExists())
         {
-            //load();
+            load();
         }
         else
         {
             addMovies();
-           // save();
+            save();
         }
+        return dateAndTimeToMovie;
     }
-
 
     private boolean repertoireAlreadyExists()
     {
-        return Files.exists(repertoireDayPath);
+        return Files.exists(repertoireFilePath);
+    }
+
+    private void load()
+    {
+        dateAndTimeToMovie.clear();
+        Stream<String> lines = getStreamOfLines(repertoireFilePath);
+        lines.forEach(line -> {
+            String[] parts = line.split("\\|");
+            LocalDateTime dateTime = LocalDateTime.parse(parts[0], DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+            Optional<Movie> movie = MovieCollection.get(Long.parseLong(parts[1]));
+            if (movie.isEmpty())
+            {
+                throw new NoSuchElementException();
+            }
+            dateAndTimeToMovie.put(dateTime, movie.get());
+        });
+        System.out.println("Repertoire has been loaded");
+    }
+
+    private Stream<String> getStreamOfLines(Path path)
+    {
+        Stream<String> lines = null;
+        try
+        {
+            lines = Files.lines(path);
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+        return lines;
     }
 
     private void addMovies()
@@ -63,34 +95,18 @@ public class Repertoire
         {
             movieHasBeenAdded = addOneMovie();
         } while (movieHasBeenAdded);
-
     }
-
 
     private boolean addOneMovie()
     {
         Movie movie = MovieCollection.getRandomMovie();
         Long movieDurationIncludingBreak = getMovieDurationWithBreak(movie);
 
-        if (minutesAreAvailable(movieDurationIncludingBreak) == true)
+        if (minutesAreAvailable(movieDurationIncludingBreak))
         {
             reduceTheNumberOfMinutesAvailable(movieDurationIncludingBreak);
-            moveTheStartTimeOfTheNextMovie(movieDurationIncludingBreak);
             dateAndTimeToMovie.put(startTimeOfTheNextMovie, movie);
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-
-    }
-
-    private boolean minutesAreAvailable(Long movieDurationIncludingBreak)
-    {
-        if (numberOfAvailableMinutes > 0)
-        {
+            moveTheStartTimeOfTheNextMovie(movieDurationIncludingBreak);
             return true;
         }
         return false;
@@ -99,8 +115,7 @@ public class Repertoire
     private Long getMovieDurationWithBreak(Movie movie)
     {
         Long breakDuration = generateBreakDurationBetweenMovies();
-        Long movieDurationWithBreak = movie.getDuration() + breakDuration;
-        return movieDurationWithBreak;
+        return movie.getDuration() + breakDuration;
     }
 
     private Long generateBreakDurationBetweenMovies()
@@ -108,37 +123,31 @@ public class Repertoire
         return ThreadLocalRandom.current().nextLong(15) + 15;
     }
 
+    private boolean minutesAreAvailable(Long movieDurationIncludingBreak)
+    {
+        return numberOfMinutesAvailable >= movieDurationIncludingBreak;
+    }
+
     private void reduceTheNumberOfMinutesAvailable(Long movieDurationIncludingBreak)
     {
-        numberOfAvailableMinutes -= movieDurationIncludingBreak;
+        numberOfMinutesAvailable -= movieDurationIncludingBreak;
     }
 
     private void moveTheStartTimeOfTheNextMovie(Long movieDurationIncludingBreak)
     {
-        startTimeOfTheNextMovie.plusMinutes(movieDurationIncludingBreak);
+        startTimeOfTheNextMovie = startTimeOfTheNextMovie.plusMinutes(movieDurationIncludingBreak);
     }
 
-
-   /* private void save()
+    private void save()
     {
-        try (PrintWriter repertoireFileWriter = new PrintWriter(Files.newBufferedWriter(repertoireDayPath)))
+        try (PrintWriter moviesFileWriter = new PrintWriter(Files.newBufferedWriter(repertoireFilePath)))
         {
-            dateAndTimeToMovie
-                    .entrySet()
-                    .stream()
-                    .filter(entry -> entry.getKey().toLocalDate().equals(possibleDateOfTheNextMovie.toLocalDate()))
-                    .map(entry -> entry.getKey() + "|" + MovieCollection.getId(entry.getValue()).get())
-                    .forEach(repertoireFileWriter::println);
+            dateAndTimeToMovie.entrySet().stream().map(entry -> entry.getKey() + "|" + MovieCollection.getId(entry.getValue()).orElseThrow()).forEach(moviesFileWriter::println);
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-        System.out.println("Movies has been saved");
+        System.out.println("Repertoire has been saved");
     }
-
-    private void load()
-    {
-
-    }*/
 }
--- src/test/java/repertoire/RepertoireTest.java
+++ src/test/java/repertoire/RepertoireTest.java
@@ -1,17 +1,62 @@
 package repertoire;
 
+import movie.Movie;
 import movie.collection.MovieCollection;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RepertoireTest
 {
+    private LocalDate date;
+    private String repertoireFilePath;
+    private Repertoire repertoire;
+
+    @BeforeEach
+    public void initialize()
+    {
+        date = LocalDate.now();
+        repertoireFilePath = "files/repertoire/" + date + ".txt";
+        repertoire = new Repertoire(date, LocalTime.of(6, 0), LocalTime.of(22, 0));
+    }
+
     @Test
-    public void g()
+    @DisplayName("Should generate and save repertoire when repertoire isn't created for this date")
+    public void shouldGenerateAndSaveRepertoireWhenRepertoireIsNotCreatedForThisDate()
     {
         MovieCollection.load();
-        Repertoire r = new Repertoire(LocalTime.of(7, 0), LocalTime.of(23, 0));
-        r.generateForTheWholeWeek();
+        Map<LocalDateTime, Movie> dateAndTimeToMovie = repertoire.generate();
+        assertFalse(dateAndTimeToMovie.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should load repertoire when repertoire is created for this date")
+    public void shouldLoadRepertoireWhenRepertoireIsCreatedForThisDate()
+    {
+        MovieCollection.load();
+        Map<LocalDateTime, Movie> dateAndTimeToMovie = repertoire.generate();
+        assertFalse(dateAndTimeToMovie.isEmpty());
+
+        dateAndTimeToMovie.clear();
+
+        dateAndTimeToMovie = repertoire.generate();
+        assertFalse(dateAndTimeToMovie.isEmpty());
+
+    }
+
+    @AfterEach
+    void cleanUp()
+    {
+        File repertoireFile = new File(repertoireFilePath);
+        if (repertoireFile.exists())
+        {
+            repertoireFile.delete();
+        }
     }
 }
