1fa49138601abd9a435e06fc9ef9548525cb5e73
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-01 00:27:20 +0100

    test for SeatCollection class done

--- src/test/java/seat/collection/SeatCollectionTest.java
+++ src/test/java/seat/collection/SeatCollectionTest.java
@@ -1,6 +1,8 @@
 package seat.collection;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import seat.Seat;
 
 import java.util.Optional;
@@ -53,7 +55,6 @@ class SeatCollectionTest
     {
         int numberOfRows = seatCollection.getRowNumberToTheNumberOfSeats().keySet().size();
         return ThreadLocalRandom.current().nextInt(numberOfRows);
-
     }
 
     @Test
@@ -66,69 +67,77 @@ class SeatCollectionTest
 
     private Integer getLastRowNumber()
     {
-        return seatCollection.getRowNumberToTheNumberOfSeats().keySet().size()-1;
+        return seatCollection.getRowNumberToTheNumberOfSeats().keySet().size() - 1;
     }
 
     @Test
     @DisplayName("Should get present seat object when number of seat is last and number of row is zero")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsLastAndNumberOfRowIsZero()
     {
-
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(0, getLastSeatNumberInRow(0));
+        assertTrue(seat.isPresent());
     }
 
-    @RepeatedTest(10)
-    @DisplayName("Should get present seat object when number of seat is last and number of row is random")
-    public void shouldGetPresentSeatObjectWhenNumberOfSeatIsLastAndNumberOfRowIsRandom()
+    private Integer getLastSeatNumberInRow(Integer rowNumber)
     {
-
+        return seatCollection.getRowNumberToTheNumberOfSeats().get(rowNumber) - 1;
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    @DisplayName("Should get present seat object when number of seat is last and number of row is random")
+    public void shouldGetPresentSeatObjectWhenNumberOfSeatIsLastAndNumberOfRowIsRandom(Integer rowNumber)
+    {
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(rowNumber, getLastSeatNumberInRow(rowNumber));
+        assertTrue(seat.isPresent());
+    }
 
     @Test
     @DisplayName("Should get present seat object when number of seat is last and number of row is last")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsLastAndNumberOfRowIsLast()
     {
-
+        Integer lastRowNumber = getLastRowNumber();
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(lastRowNumber, getLastSeatNumberInRow(lastRowNumber));
+        assertTrue(seat.isPresent());
     }
 
     @RepeatedTest(20)
     @DisplayName("Should get present seat object when number of seat is random and number of row is random")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsRandomAndNumberOfRowIsRandom()
     {
+        Integer randomRowNumber = generateRandomRowNumber();
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(randomRowNumber, generateRandomSeatNumberInDefinedRow(randomRowNumber));
+        assertTrue(seat.isPresent());
+    }
 
+
+    private Integer generateRandomSeatNumberInDefinedRow(Integer rowNumber)
+    {
+        int numberOfSeat = seatCollection.getRowNumberToTheNumberOfSeats().get(rowNumber);
+        return ThreadLocalRandom.current().nextInt(numberOfSeat);
     }
 
     @Test
-    @DisplayName("Should get present seat object when number of seat is wrong and number of row is find")
+    @DisplayName("Should get empty seat object when number of seat is wrong and number of row is find")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsWrongAndNumberOfRowIsFind()
     {
-
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(0, Integer.MIN_VALUE);
+        assertTrue(seat.isEmpty());
     }
+
     @Test
-    @DisplayName("Should get present seat object when number of seat is find and number of row is wrong")
+    @DisplayName("Should get empty seat object when number of seat is find and number of row is wrong")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsFindAndNumberOfRowIsWrong()
     {
-
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(Integer.MIN_VALUE, 0);
+        assertTrue(seat.isEmpty());
     }
 
     @Test
-    @DisplayName("Should get present seat object when number of seat is wrong and number of row is wrong")
+    @DisplayName("Should get empty seat object when number of seat is wrong and number of row is wrong")
     public void shouldGetPresentSeatObjectWhenNumberOfSeatIsWrongAndNumberOfRowIsWrong()
     {
-
+        Optional<Seat> seat = seatCollection.getSeatByRowNumberAndSeatNumber(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        assertTrue(seat.isEmpty());
     }
-
-    /*
-
-    private Integer generateRandomRowNumber()
-    {
-        int numberOfRows = seatCollection.getRowNumberToTheNumberOfSeats().keySet().size();
-        return ThreadLocalRandom.current().nextInt(numberOfRows);
-    }
-
-    private Integer generateRandomSeatNumber(Integer rowNumber)
-    {
-        int numberOfSeats = seatCollection.getRowNumberToTheNumberOfSeats().get(rowNumber);
-        return ThreadLocalRandom.current().nextInt(numberOfSeats);
-    }*/
 }
