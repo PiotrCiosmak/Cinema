bcc36c8a195b5278a0f5d8f8055d5279227334f3
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-28 15:20:00 +0100

    enum stores information about price multiplier

--- src/main/java/seat/type/Type.java
+++ src/main/java/seat/type/Type.java
@@ -2,7 +2,20 @@ package seat.type;
 
 public enum Type
 {
-    STANDARD,
-    COMFORTABLE,
-    VIP
+    STANDARD(1.0),
+    COMFORTABLE(1.3),
+    VIP(1.9);
+
+    private final Double priceMultiplier;
+
+    Type(double priceMultiplier)
+    {
+        this.priceMultiplier = priceMultiplier;
+    }
+
+    public Double getPriceMultiplier()
+    {
+        return priceMultiplier;
+    }
+
 }
