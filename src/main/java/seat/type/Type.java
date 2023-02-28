c1e1b239436593d97343c63e8298b1eea15f31e9
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-28 16:30:43 +0100

    method generateType added

--- src/main/java/seat/type/Type.java
+++ src/main/java/seat/type/Type.java
@@ -1,5 +1,7 @@
 package seat.type;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public enum Type
 {
     STANDARD(1.0),
@@ -18,4 +20,18 @@ public enum Type
         return priceMultiplier;
     }
 
+    public static Type generateType()
+    {
+        double randomNumberOnTheBasisOfWhichTheSeatTypeIsDetermined = ThreadLocalRandom.current().nextDouble();
+
+        if (randomNumberOnTheBasisOfWhichTheSeatTypeIsDetermined < 0.1)
+        {
+            return Type.VIP;
+        }
+        if (randomNumberOnTheBasisOfWhichTheSeatTypeIsDetermined < 0.4)
+        {
+            return Type.COMFORTABLE;
+        }
+        return Type.STANDARD;
+    }
 }
